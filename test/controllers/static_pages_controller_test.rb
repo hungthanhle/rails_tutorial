f67594ace9dba2f74a7b5961dbb178264de60e79
hung <hunglt.ee@gmail.com>
2023-03-24 09:50:58 +0700

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -34,7 +34,8 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "full title helper" do
-    assert_equal full_title, "Help | #{@base_title}"
-    assert_equal full_title("Help"), "Help | #{@base_title}"
-    end
+    get helf_path
+    assert_equal full_title, "Ruby on Rails Tutorial Sample App"
+    assert_equal full_title("Help"), "Help | Ruby on Rails Tutorial Sample App"
+  end
 end
