d610ec713a08e2ce15d469ceb8acb78f07498703
hung <hunglt.ee@gmail.com>
2023-03-29 16:57:52 +0700

    logged in edit

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -6,6 +6,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
   
   test "unsuccessful edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
     # assert_template 'users/edit'
     patch user_path(@user), params: { user: { name: "", email: "foo@invalid", password: "foo", password_confirmation: "bar" } }
@@ -13,6 +14,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "successful edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
     # assert_template 'users/edit'
     name = "Foo Bar"
@@ -23,6 +25,18 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     @user.reload
     assert_equal name, @user.name
     assert_equal email, @user.email
+  end
+
+  test "should redirect edit when not logged in" do
+      get edit_user_path(@user)
+      assert_not flash.empty?
+      assert_redirected_to login_url
+  end
+
+  test "should redirect update when not logged in" do
+    patch user_path(@user), params: { user: { name: @user.name, email: @user.email } }
+    assert_not flash.empty?
+    assert_redirected_to login_url
   end
   # test "the truth" do
   #   assert true
