bad582d9146af3f8a1e6090471ec63384efa96a5
hung <hunglt.ee@gmail.com>
2023-03-30 13:37:21 +0700

    admin column

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,3 +20,20 @@ User.create!(name: "Example User",
   password: password,
   password_confirmation: password)
 end
+
+# Create a main sample user.
+User.create!(name: "Example User",
+  email: "exampleAAA@railstutorial.org",
+  password: "foobar",
+  password_confirmation: "foobar",
+  admin: true)
+# Generate a bunch of additional users.
+99.times do |n|
+  name = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name: name,
+  email: email,
+  password: password,
+  password_confirmation: password)
+end
