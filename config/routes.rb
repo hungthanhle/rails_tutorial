975716bc751725e355ac76eedebea58e0380e296
hung <hunglt.ee@gmail.com>
2023-04-03 14:39:49 +0700

    home post micropost

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,5 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help', as: 'helf'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
+  resources :microposts, only: [:create, :destroy]
 end
