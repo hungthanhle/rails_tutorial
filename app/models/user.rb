54bb45fa4bb5e49447c7aa11b511085e83308742
hung <hunglt.ee@gmail.com>
2023-04-04 16:06:21 +0700

    feed scale

--- app/models/user.rb
+++ app/models/user.rb
@@ -78,8 +78,11 @@ class User < ApplicationRecord
   def feed
     # microposts
     # Micropost.where("user_id = ?", id)
-    
-    Micropost.where("user_id IN (?) OR user_id = ?", following_ids, id)
+
+    # Micropost.where("user_id IN (?) OR user_id = ?", following_ids, id)
+    following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+    # Micropost.where("user_id IN (:following_ids) OR user_id = :user_id", following_ids: following_ids, user_id: id)
+    Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
   end
 
   # Follows a user.
