c65b45752486ba1b8696cd5ced77883383116883
hung <hunglt.ee@gmail.com>
2023-04-06 13:37:35 +0700

    asset precompile one page

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,3 +8,4 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "jquery", to: "https://code.jquery.com/jquery-3.3.1.slim.min.js", preload: true
 pin "@popperjs/core", to: "https://unpkg.com/@popperjs/core@2.11.6/dist/esm/index.js", preload: true
 pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js", preload: true
+# pin "jscontroller", to: "app/javascript/js_controller", preload: true # không import
