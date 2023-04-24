9df02415318dadb8258aa1ea5af812bb198e55fe
hung <hunglt.ee@gmail.com>
2023-04-24 13:51:52 +0700

    review send mail feature

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,11 +60,23 @@ Rails.application.configure do
   config.assets.quiet = true
 
   config.action_mailer.raise_delivery_errors = false
-  host = 'example.com' # Don't use this literally; use your local dev host instead
+  host = 'localhost:3000' # Don't use this literally; use your local dev host instead
   # Use this on the cloud IDE.
-  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+  # config.action_mailer.default_url_options = { host: host, protocol: 'https' }
   # Use this if developing on localhost.
-  # config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'localhost',
+    user_name:            ENV['GMAIL_USERNAME'],
+    password:             ENV['GMAIL_APPLICATION_PASS'],
+    authentication:       'plain',
+    enable_starttls_auto: true  
+  }
   
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
---
+++ config/initializers/setup_mail.rb
@@ -0,0 +1,11 @@
+# ActionMailer::Base.delivery_method = :smtp
+
+# ActionMailer::Base.smtp_settings = {
+#   :address              => "smtp.gmail.com",
+#   :port                 => "587",
+#   :domain               => "localhost",
+#   :user_name            => ENV['gmail_username'],
+#   :password             => ENV['gmail_password'],
+#   :authentication       => "plain",
+#   :enable_starttls_auto => true
+# }
