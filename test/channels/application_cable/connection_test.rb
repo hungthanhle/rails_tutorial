cc0821bb789fa474232b82889b62239322076f73
hung <hunglt.ee@gmail.com>
2023-04-28 09:46:45 +0700

    first draft test

--- test/channels/application_cable/connection_test.rb
+++ test/channels/application_cable/connection_test.rb
@@ -1,11 +1,24 @@
 require "test_helper"
 
 class ApplicationCable::ConnectionTest < ActionCable::Connection::TestCase
-  # test "connects with cookies" do
-  #   cookies.signed[:user_id] = 42
-  #
+  def setup
+    @user = users(:michael)
+    @other_user = users(:archer)
+  end
+
+  # test "connect" do
+  #   = a || b
+  #   cookies.encrypted["user_id"] = @user.id
   #   connect
-  #
-  #   assert_equal connection.user_id, "42"
+  #   assert_equal connection.current_user.id, @user.id
   # end
+
+  test "connect" do
+    connect session: {user_id: @user.id}
+    assert_equal connection.current_user.id, @user.id
+  end
+
+  test "connection rejected without log in" do
+    assert_reject_connection { connect }
+  end
 end
