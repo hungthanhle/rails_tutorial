00de0d377a8c204192c4b453058e0ced74288fa1
hung <hunglt.ee@gmail.com>
2023-05-12 10:52:19 +0700

    I18n for layouts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+  # before_action :set_locale
+  around_action :switch_locale
+
   include SessionsHelper
   def hello
     render html: "hello, world!"
@@ -11,6 +14,26 @@ class ApplicationController < ActionController::Base
         store_location
         flash[:danger] = "Please log in."
         redirect_to login_url
+      end
+    end
+
+    # def set_locale
+    #   I18n.locale = params[:locale] || I18n.default_locale
+    # end
+
+    # locale for next url
+    def default_url_options
+      {locale: I18n.locale}
+    end
+
+    def switch_locale(&action)
+      locale = available_locale
+      I18n.with_locale(locale, &action)
+    end
+
+    def available_locale
+      if params[:locale]
+        I18n.available_locales.include?(params[:locale].to_sym)? params[:locale] : I18n.default_locale
       end
     end
 end
